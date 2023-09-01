73499e9e60b35c17d9ce11efd249b691a6cc8945
Gathsara2002 <liviru2002mmg@gmail.com>
2023-09-01 20:31:48 +0530

    player is moving with keys

--- JS_2D_Game/assets/js/level1.js
+++ JS_2D_Game/assets/js/level1.js
@@ -1,3 +1,6 @@
+let bottom=10;
+let right=0;
+
 $(window).keydown(function (e) {
     /*event for pressing space bar*/
     if (e.keyCode === 32) {
@@ -7,23 +10,31 @@ $(window).keydown(function (e) {
 
     /*up arrow or w key event */
     if (e.keyCode === 38 || e.keyCode === 87) {
-        alert("w , up");
+        //alert("w , up");
+        bottom=bottom+10;
     }
 
     /*down arrow or s key event */
     if (e.keyCode === 40 || e.keyCode === 83) {
-        alert("s , down");
+        //alert("s , down");
+        bottom=bottom-10;
     }
 
     /*left arrow or a key event */
     if (e.keyCode === 37 || e.keyCode === 65) {
-        alert("a , left");
+        //alert("a , left");
+        right=right+20;
     }
 
     /*right arrow or d key event */
     if (e.keyCode === 39 || e.keyCode === 68) {
-        alert("d , right");
+        //alert("d , right");
+        right=right-20;
     }
+
+    /*move player*/
+    $("#player").css('bottom',bottom+'px');
+    $("#player").css('right',right+'px');
 });
 
 
@@ -44,14 +55,12 @@ function setScore() {
 
 /*pause game */
 $("#pause").click(function () {
-    console.log("paused");
     pauseScore=true;
     setScore();
 });
 
 /*restart game*/
 $("#restart").click(function () {
-    console.log("restart");
     pauseScore=false;
     setScore();
 });
