be8097d5c63180cf69c83d0356e18de4fdbdc02d
Gathsara2002 <liviru2002mmg@gmail.com>
2023-09-21 20:56:18 +0530

    added click event

--- JS_2D_Game/assets/js/myScript.js
+++ JS_2D_Game/assets/js/myScript.js
@@ -1,1 +1,15 @@
 console.log("hi");
+
+let clickCount = 0;
+
+$("#description").css('display', 'none');
+
+$("#instruction").click(function () {
+    if (clickCount === 0) {
+        $("#description").css('display', 'block');
+        clickCount++;
+    } else if (clickCount === 1) {
+        $("#description").css('display', 'none');
+        clickCount = 0;
+    }
+});
